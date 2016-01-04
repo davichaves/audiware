998ebcdea90d14858e18bfebc6db9fe289ec9094
Davi Chaves <dchaves@usc.edu>
2016-01-04 16:44:45 -0300

    adding stop button

--- Audiware/ViewController.swift
+++ Audiware/ViewController.swift
@@ -10,16 +10,35 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var text: UILabel!
+    
+    @IBOutlet weak var stop: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        stop.hidden = true;
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    
+    @IBAction func recordAudio(sender: UIButton) {
+        text.hidden = false //show text
+        stop.hidden = false //show button
+        print("record")
+    }
 
+    @IBAction func stopRecording(sender: UIButton) {
+        text.hidden = true
+        print("stop")
+    }
 
 }
 
