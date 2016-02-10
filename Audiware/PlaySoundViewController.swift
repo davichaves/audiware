9fddb0704610f32a944999a306fdc5b4b713d8bc
Davi Chaves <dchaves@usc.edu>
2016-02-10 03:45:40 -0800

    chipmunk effect working

--- Audiware/PlaySoundViewController.swift
+++ Audiware/PlaySoundViewController.swift
@@ -14,18 +14,29 @@ class PlaySoundViewController: UIViewController {
     
     var audioPlayer:AVAudioPlayer!
     var receivedAudio:RecordedAudio!
+    var audioEngine:AVAudioEngine!
+    var audioFile:AVAudioFile!
     
     override func viewDidLoad() {
-        if let filePath = NSBundle.mainBundle().pathForResource("movie_quote", ofType: "mp3") {
-            let fileUrl = NSURL.fileURLWithPath(filePath);
-            do {
-                audioPlayer = try AVAudioPlayer(contentsOfURL: fileUrl);
-            } catch {
-                print("problem in AVAudioPlayer");
-            };
-        } else {
-            print("the filepath is empty");
+//        if let filePath = NSBundle.mainBundle().pathForResource("movie_quote", ofType: "mp3") {
+//            let fileUrl = NSURL.fileURLWithPath(filePath);
+//            do {
+//                
+//            } catch {
+//                print("problem in AVAudioPlayer");
+//            };
+//        } else {
+//            print("the filepath is empty");
+//        }
+        do {
+            audioPlayer = try AVAudioPlayer(contentsOfURL: receivedAudio.filePathUrl);
+        } catch {
+            print("problem in AVAudioPlayer");
         }
+        audioPlayer.enableRate = true;
+        
+        audioEngine = AVAudioEngine();
+        audioFile = try! AVAudioFile(forReading: receivedAudio.filePathUrl)
     }
 
     override func didReceiveMemoryWarning() {
@@ -40,10 +51,34 @@ class PlaySoundViewController: UIViewController {
     @IBAction func playFast(sender: UIButton) {
         playAudio(1.5);
     }
+
+    @IBAction func playChipmunk(sender: UIButton) {
+        playAudioWithVariablePitch(1000);
+    }
+    
+    func playAudioWithVariablePitch(pitch: Float) {
+        audioPlayer.stop();
+        audioEngine.stop();
+        audioEngine.reset();
+        
+        let audioPlayerNode = AVAudioPlayerNode();
+        audioEngine.attachNode(audioPlayerNode);
+        
+        let changePitchEffect = AVAudioUnitTimePitch();
+        changePitchEffect.pitch = pitch;
+        audioEngine.attachNode(changePitchEffect)
+        
+        audioEngine.connect(audioPlayerNode, to: changePitchEffect, format: nil)
+        audioEngine.connect(changePitchEffect, to: audioEngine.outputNode, format: nil)
+        
+        audioPlayerNode.scheduleFile(audioFile, atTime: nil, completionHandler: nil);
+        try! audioEngine.start();
+        
+        audioPlayerNode.play();
+    }
     
     func playAudio(myRate: Float) {
         audioPlayer.stop();
-        audioPlayer.enableRate = true;
         audioPlayer.rate = myRate;
         audioPlayer.currentTime = 0.0;
         audioPlayer.play();
